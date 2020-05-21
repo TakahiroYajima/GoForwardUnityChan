488fa6fd027fb1526956fc9c9aed17c81f93f59c
TakahiroYajima <yajimatakahiro@outlook.jp>
2020-05-21 22:47:58 +0900

    Lesson7課題提出
    普通課題。ブロックの音を鳴らす

---
+++ Assets/CubeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    [SerializeField] private AudioSource seAudio = null;
+    [SerializeField] private AudioClip blockSE = null;
+
+    private float speed = -12;
+
+    private float deadLine = -10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        if(transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "BlockTag" || collision.gameObject.tag == "GroundTag")
+        {
+            seAudio.PlayOneShot(blockSE);
+        }
+    }
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+
+    public GameObject gameOverText;
+    public GameObject runLingthText;
+
+    private float len = 0;
+    private float speed = 0.03f;
+
+    private bool isGameOver = false;
+
+	// Use this for initialization
+	void Start () {
+        this.gameOverText = GameObject.Find("GameOver");
+        this.runLingthText = GameObject.Find("RunLength");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(this.isGameOver == false)
+        {
+            this.len += this.speed;
+            this.runLingthText.GetComponent<Text>().text = "Distance:  " + len.ToString("F2") + "m";
+        }
+        if(this.isGameOver == true)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
+	}
+
+    public void GameOver()
+    {
+        this.gameOverText.GetComponent<Text>().text = "GameOver";
+        this.isGameOver = true;
+    }
+}
